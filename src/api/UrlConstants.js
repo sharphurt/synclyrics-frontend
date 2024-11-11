9ae7a502c05c8a4dcdddd0cfedac52eb285455c6
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-11-12 01:54:12 +0500

    bugfix: changed backend port

--- src/api/UrlConstants.js
+++ src/api/UrlConstants.js
@@ -1,6 +1,6 @@
 export const UrlConstants = {
     SpotifyUrl: "https://api.spotify.com/v1",
-    LyricsUrl: "http://localhost:8080/lyrics",
-    SessionUrl: "http://localhost:8080/session",
-    LoginUrl: "http://localhost:8080/login"
+    LyricsUrl: "http://localhost:8104/lyrics",
+    SessionUrl: "http://localhost:8104/session",
+    LoginUrl: "http://localhost:8104/login"
 }
