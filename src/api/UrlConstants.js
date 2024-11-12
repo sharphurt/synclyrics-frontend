b383f51f36c41c4e9bdc54cdfb87f718db5f948a
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-11-12 17:01:14 +0500

    bugfix: changed backend host url

--- src/api/UrlConstants.js
+++ src/api/UrlConstants.js
@@ -1,6 +1,6 @@
 export const UrlConstants = {
     SpotifyUrl: "https://api.spotify.com/v1",
-    LyricsUrl: "http://localhost:8104/lyrics",
-    SessionUrl: "http://localhost:8104/session",
-    LoginUrl: "http://localhost:8104/login"
+    LyricsUrl: "https://sharphurt.ru:8104/lyrics",
+    SessionUrl: "https://sharphurt.ru:8104/session",
+    LoginUrl: "https://sharphurt.ru:8104/login"
 }
