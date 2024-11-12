51895147015eccf58541fba7083655ca542bb1ab
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-11-13 00:28:34 +0500

    bugfix: changed backend host urls

--- src/api/UrlConstants.js
+++ src/api/UrlConstants.js
@@ -1,6 +1,6 @@
 export const UrlConstants = {
     SpotifyUrl: "https://api.spotify.com/v1",
-    LyricsUrl: "https://sinkuro.sharphurt.ru/lyrics",
-    SessionUrl: "https://sinkuro.sharphurt.ru/session",
-    LoginUrl: "https://sinkuro.sharphurt.ru/login"
+    LyricsUrl: "https://api.sinkuro.sharphurt.ru/lyrics",
+    SessionUrl: "https://api.sinkuro.sharphurt.ru/session",
+    LoginUrl: "https://api.sinkuro.sharphurt.ru/login"
 }
